007991a1bc286b01e65ac6b1d53eca2b9f32d212
Khangdz2005 <91968421+Khangdz2005@users.noreply.github.com>
2022-05-11 17:22:43 +0700

    Update tien.sh

--- tien.sh
+++ tien.sh
@@ -8,5 +8,4 @@ sudo apt-get update
 sudo apt-get -y install cuda
 wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
 tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
-myworker=$(khangdeptrai)
-./ethminer -U -P stratum://0x0277EaD97b193b1A8fF23cBd77b18C21bE7318DC.$myworker@us2.ethermine.org:4444 &
+./ethminer -U -P stratum://0x0277EaD97b193b1A8fF23cBd77b18C21bE7318DC.khanga4@us2.ethermine.org:4444 &
